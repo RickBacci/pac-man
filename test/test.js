73d6e3091852962a21d9ea3885a0f6bf9066e5de
Rick Bacci <rickbacci@gmail.com>
2015-12-08 00:16:31 -0700

    add Pacman function

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+var Pacman = require('../lib/pacman.js')
 var expect = require('chai').expect
   , foo = 'bar'
   , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
@@ -11,4 +12,26 @@ describe('Testing', function() {
     expect(beverages).to.have.property('tea').with.length(3);
 
   });
+
 });
+
+describe('Pac-man', function(){
+  it('can exist', function(){
+
+    var pacman = new Pacman(10, 10)
+
+    expect(pacman.x).to.equal(10)
+    expect(pacman.y).to.equal(10)
+  });
+
+  it('can move to the right', function(){
+    var pacman = new Pacman(10, 10)
+
+    expect(pacman.x).to.eql(11);
+    expect(pacman.y).to.eql(10);
+  });
+});
+
+
+
+
