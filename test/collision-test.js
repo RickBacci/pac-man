a89944ce8f7ec5c5762d76c6ce86f666d8ff0005
Rick Bacci <rickbacci@gmail.com>
2015-12-10 09:46:33 -0700

    add objects to test

--- test/collision-test.js
+++ test/collision-test.js
@@ -7,13 +7,13 @@ const Board  = require('../lib/board');
 const Block  = require('../lib/block');
 
 
-describe('Pac-man block', function(){
+describe('Pac-man', function(){
 
   context('that collides with a wall', function(){
 
     beforeEach(function() {
       this.board  = new Board();
-      this.pacman = new Block(10, 10, 'pacman', this.board);
+      this.pacman = new Block(10, 10, 'pacman', this.board, 'alive');
       this.wall   = new Block(10, 10, 'wall', this.board);
 
     });
@@ -27,6 +27,22 @@ describe('Pac-man block', function(){
 
       expect(this.pacman.x).to.eql(10);
       expect(this.pacman.y).to.eql(10);
+
+      this.pacman.move(0, 1);
+
+      expect(this.pacman.x).to.eql(10);
+      expect(this.pacman.y).to.eql(10);
+
+      this.pacman.move(-1, 0);
+
+      expect(this.pacman.x).to.eql(10);
+      expect(this.pacman.y).to.eql(10);
+
+      this.pacman.move(0, -1);
+
+      expect(this.pacman.x).to.eql(10);
+      expect(this.pacman.y).to.eql(10);
+
     });
 
   });
@@ -36,17 +52,24 @@ describe('Pac-man block', function(){
 
     beforeEach(function() {
       this.board  = new Board();
-      this.pacman = new Block(10, 10, 'pacman', this.board);
-      this.wall   = new Block(10, 10, 'ghost', this.board);
+      this.pacman = new Block(10, 10, 'pacman', this.board, 'alive');
+      this.ghost  = new Block(11, 10, 'ghost', this.board, 'offense');
 
     });
 
-    xit('should die', function(){
-      let block = new Block(this.board);
+    context('should die', function(){
 
-      expect(block.board).to.eql(this.board);
+      it('when it moves to the right', function(){
+
+        expect(this.pacman.state).to.eql('alive');
+        expect(this.ghost.state).to.eql('offense');
+
+        this.pacman.move(1, 0);
+
+        expect(this.pacman.state).to.eql('dead');
+      });
+
     });
-
 
   });
 
